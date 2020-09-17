45f3d00083e795fa5b789badff3f7ec2a5e5cf87
Russell Juma <32531681+RussellJuma@users.noreply.github.com>
2020-09-16 18:42:26 -0700

    1.0

--- README.md
+++ README.md
@@ -1,1 +1,179 @@
 # PySuiteCRM
+# PySuiteCRM   
+[![GitHub issues](https://img.shields.io/github/issues/RussellJuma/PySuiteCRM)](https://github.com/RussellJuma/PySuiteCRM/issues)
+[![GitHub stars](https://img.shields.io/github/stars/RussellJuma/PySuiteCRM)](https://github.com/RussellJuma/PySuiteCRM/stargazers)
+[![GitHub license](https://img.shields.io/github/license/RussellJuma/PySuiteCRM)](https://github.com/RussellJuma/PySuiteCRM/blob/master/LICENSE)
+
+PySuiteCRM ultilizes the SuiteCRM V8 API via Oauth2
+
+PySuiteCRM supports all versions of SuiteCRM `7.10+`
+
+## Contents
+
+- [Installation](#Installation)
+    - [OAuth2 Setup](#OAuth2 Setup)
+    - [SuiteCRM Setup](#SuiteCRM Setup)
+    - [PySuiteCRM Setup](#PySuiteCRM Setup)
+- [Configuration](#Configuration)
+- [Usage](#Usage)
+    - [Create](#Create)
+    - [Update](#Update)
+    - [Get](#Get)
+    - [Get All](#Get All)  
+    - [Delete](#Delete)
+    - [Create Relationship](#Create Relationship)
+    - [Get Relationship](#Get Relationship)
+    - [Delete Relationship](#Delete Relationship)
+    - [Fields](#Fields)
+- [Performance](#Performance)
+- [Contributing](#Contributing)
+- [Credits](#Credits)
+- [License](#License)
+
+## Installation
+### OAuth2 Setup
+[SuiteCRM Oauth2 Setup source](https://docs.suitecrm.com/developer/api/developer-setup-guide/json-api/#_generate_private_and_public_key_for_oauth2)
+
+SuiteCRM Api uses OAuth2 protocol, which needs public and private keys.
+
+First, open a terminal and go to 
+```
+{{suitecrm.root}}/Api/V8/OAuth2
+```
+
+Generate a private key:
+```bash
+openssl genrsa -out private.key 2048
+```
+
+Generate a public key:
+```bash
+openssl rsa -in private.key -pubout -out public.key
+```
+If you need more information about generating, [please visit this page](https://oauth2.thephpleague.com/installation/).
+
+The permission of the key files must be 600 or 660, so change it.
+```bash
+sudo chmod 600 private.key public.key
+```
+
+Make sure that the config files are owned by PHP
+```bash
+sudo chown www-data:www-data p*.key
+```
+
+OAuth2’s Authorization Server needs to set an encryption key for security reasons. This key has been gererated during the SuiteCRM installation and stored in the config.php under "oauth2_encryption_key". If you would like to change its value you may generate a new one by running and then storing the output in the config.php.
+```bash
+echo base64_encode(random_bytes(32)).PHP_EOL;
+```
+If you need more information about this issue, [please visit this page](https://oauth2.thephpleague.com/v5-security-improvements/)
+
+<br/>
+
+### SuiteCRM Setup
+Login as Admin and navigate to Admin>OAuth2 Clients and Tokens>New Client Credentials Client and generate Client Credentials.
+Use the client id and client secret in the [Config.ini](\Config.ini)
+
+## PySuiteCRM Setup
+Run the following command inside the directory of SuiteCRMPy
+```bash
+pip install -r requirements.txt
+```
+## Configuration
+
+Open [Config.ini](\Config.ini)
+```bash
+headers = Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36
+baseurl = https://{your SuiteCRM url}
+client id = 
+client secret = 
+logout on exit = False
+access token = 
+# Default cache requests = False
+# See single source of truth (SSOT) before setting cache requests to True
+cache requests = False
+# Default cache is set to 5 minutes
+cache timeout seconds = 300
+```
+
+## Usage
+
+###Import
+```python
+from SuiteCRM import SuiteCRM
+
+suitecrm = SuiteCRM()
+```
+
+###Create
+```python
+result = suitecrm.Contacts.create(title='Software Engineer', first_name='Russell', last_name='Juma')
+```
+
+###Update
+```python
+result = suitecrm.Contacts.update(id='11129071-da4c-18ef-3107-5ead3a71d6fe', account_id='555-555-5555')
+```
+
+###Get
+```python
+# Request a record by id, returns a single record.
+result = suitecrm.Contacts.get(id='11129071-da4c-18ef-3107-5ead3a71d6fe')
+
+# Filter records by first and last name, returns a list of records.
+result = suitecrm.Contacts.get(first_name='Russell', last_name='Juma')
+
+# Filter records by first name, sort on last name, and only return full name and mobile phone in the records.
+result = suitecrm.Contacts.get(fields=['full_name', 'phone_mobile'], first_name= 'Sarah', sort='last_name')
+
+# return all records in a given module, default will pull 100 records per Get request to API.
+result = suitecrm.Contacts.get_all()
+
+```
+####Limitations
+Get cannot filter on custom fields due to [bug #7285](https://github.com/salesagility/SuiteCRM/issues/7285) in SuiteCRM.
+
+###Delete
+```python
+# Delete record by id
+result = suitecrm.Contacts.delete(id='11129071-da4c-18ef-3107-5ead3a71d6fe')
+```
+
+###Create Relationship
+```python
+# Create relationship between '11129071-da4c-18ef-3107-5ead3a71d6fe' in the Contacts and Accounts with id ='555-555-5555'
+result = suitecrm.Contacts.create_relationship('11129071-da4c-18ef-3107-5ead3a71d6fe', 'Accounts', '555-555-5555')
+```
+###Get Relationship
+```python
+# Get relationships between '11129071-da4c-18ef-3107-5ead3a71d6fe' in the Contacts with any in Accounts.
+result = suitecrm.Contacts.get_relationship('11129071-da4c-18ef-3107-5ead3a71d6fe', 'Accounts')
+```
+###Delete Relationship
+```python
+# Delete relationship between '11129071-da4c-18ef-3107-5ead3a71d6fe' in the Contacts and Accounts with id ='555-555-5555'
+result = suitecrm.Contacts.delete('11129071-da4c-18ef-3107-5ead3a71d6fe', 'Accounts', '555-555-5555')
+```
+
+###Fields
+```python
+# Returns all the attributes in a module that can be set.
+result = suitecrm.Contacts.fields()
+
+['name', 'date_entered', 'date_modified', 'etc...']
+```
+
+## Performance
+With Cache set to `True`, all Get, Create, Update requests are stored local within the module's cache. Cache is only 
+pulled went the specific id is given,ie. `get(suitecrm.Contacts.id='11129071-da4c-18ef-3107-5ead3a71d6fe')` 
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## Credits
+- [Russell Juma](https://github.com/RussellJuma)
+
+## License
+PySuiteCRM is open source software licensed under the MIT license. See [LICENSE.md](LICENSE.md) for more information.
---
+++ SuiteCRM.py
@@ -0,0 +1,354 @@
+import json
+import uuid
+import configparser
+import atexit
+import math
+import datetime
+from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2 import BackendApplicationClient, TokenExpiredError, InvalidClientError
+from oauthlib.oauth2.rfc6749.errors import CustomOAuth2Error
+
+
+class SuiteCRM(object):
+
+    def __init__(self):
+        self._config()
+        self._login()
+        self._modules()
+
+    def _modules(self):
+        self.Accounts = Module(self, 'Accounts')
+        self.Bugs = Module(self, 'Bugs')
+        self.Calendar = Module(self, 'Calendar')
+        self.Calls = Module(self, 'Calls')
+        self.Cases = Module(self, 'Cases')
+        self.Campaigns = Module(self, 'Campaigns')
+        self.Contacts = Module(self, 'Contacts')
+        self.Documents = Module(self, 'Documents')
+        self.Email = Module(self, 'Email')
+        self.Emails = Module(self, 'Emails')
+        self.Employees = Module(self, 'Employees')
+        self.Leads = Module(self, 'Leads')
+        self.Lists = Module(self, 'Lists')
+        self.Meetings = Module(self, 'Meetings')
+        self.Notes = Module(self, 'Notes')
+        self.Opportunities = Module(self, 'Opportunities')
+        self.Projects = Module(self, 'Projects')
+        self.Spots = Module(self, 'Spots')
+        self.Surveys = Module(self, 'Surveys')
+        self.Target = Module(self, 'Target')
+        self.Targets = Module(self, 'Targets')
+        self.Tasks = Module(self, 'Tasks')
+        self.Templates = Module(self, 'Templates')
+
+    def _config(self):
+        self.config = configparser.ConfigParser()
+        self.config.read('Config.ini')
+        self.baseurl = self.config['SUITECRM']['base url']
+
+    def _refresh_token(self):
+        """
+        Fetch a new token from from token access url, specified in config file.
+        :return: None
+        """
+        try:
+            self.OAuth2Session.fetch_token(token_url=self.config['SUITECRM']['token url'],
+                                           client_id=self.config['SUITECRM']['client id'],
+                                           client_secret=self.config['SUITECRM']['client secret'])
+        except InvalidClientError:
+            exit('401 (Unauthorized) - client id/secret')
+        except CustomOAuth2Error:
+            exit('401 (Unauthorized) - client id')
+        # Update configuration file with new token'
+        self.config.set('SUITECRM', 'Access Token', str(self.OAuth2Session.token))
+        with open('Config.ini', 'w') as configfile:
+            self.config.write(configfile)
+
+    def _login(self):
+        """
+        Checks to see if a Oauth2 Session exists, if not builds a session and retrieves the token from the config file,
+        if no token in config file, fetch a new one.
+
+        :return: None
+        """
+        # Does session exist?
+        if not hasattr(self, 'OAuth2Session'):
+            client = BackendApplicationClient(client_id=self.config['SUITECRM']['Client ID'])
+            self.OAuth2Session = OAuth2Session(client=client,
+                                               client_id=self.config['SUITECRM']['Client ID'])
+            self.OAuth2Session.headers.update({"User-Agent": self.config['SUITECRM']['Headers'],
+                                               'Content-Type': 'application/json'})
+            if self.config['SUITECRM']['access token'] == '':
+                self._refresh_token()
+            else:
+                self.OAuth2Session.token = eval(self.config['SUITECRM']['access token'])
+        else:
+            self._refresh_token()
+
+        # Logout on exit
+        if eval(self.config['SUITECRM']['logout on exit']):
+            atexit.register(self._logout)
+
+    def _logout(self):
+        """
+        Logs out current Oauth2 Session
+        :return: None
+        """
+        url = '/logout'
+        self.request(f'{self.baseurl}{url}', 'post')
+        self.config.set('SUITECRM', 'access token', '')
+        with open('Config.ini', 'w') as configfile:
+            self.config.write(configfile)
+
+    def request(self, url, method, parameters=''):
+        """
+        Makes a request to the given url with a specific method and data. If the request fails because the token expired
+        the session will re-authenticate and attempt the request again with a new token.
+
+        :param url: (string) The url
+        :param method: (string) Get, Post, Patch, Delete
+        :param parameters: (dictionary) Data to be posted
+
+        :return: (dictionary) Data
+        """
+        data = json.dumps({"data": parameters})
+        try:
+            the_method = getattr(self.OAuth2Session, method)
+        except AttributeError:
+            return
+
+        try:
+            if parameters == '':
+                data = the_method(url)
+            else:
+                data = the_method(url, data=data)
+        except TokenExpiredError:
+            self._refresh_token()
+            if parameters == '':
+                data = the_method(url)
+            else:
+                data = the_method(url, data=data)
+
+        # Revoked Token
+        attempts = 0
+        while data.status_code == 401 and attempts < 1:
+            self._refresh_token()
+            if parameters == '':
+                data = the_method(url)
+            else:
+                data = the_method(url, data=data)
+            attempts += 1
+        if data.status_code == 401:
+            exit('401 (Unauthorized) client id/secret has been revoked, new token was attempted and failed.')
+
+        # Database Failure
+        # SuiteCRM does not allow to query by a custom field see README, #Limitations
+        if data.status_code == 400 and 'Database failure.' in data.content.decode():
+            raise Exception(data.content.decode())
+
+        return json.loads(data.content)
+
+
+class Module:
+
+    def __init__(self, suitecrm, module_name):
+        self.module_name = module_name
+        self.suitecrm = suitecrm
+        self.cache = {}
+        self.cache_time = {}
+        self.cache_status = eval(self.suitecrm.config['SUITECRM']['cache requests'])
+        self.cache_timeout_seconds = eval(self.suitecrm.config['SUITECRM']['cache timeout seconds'])
+
+    def _cache_delete(self, **by):
+        """
+        Clears cache by define by an option
+        :param by: (string) Option of all,id, or old cache
+        :return: None
+        """
+        if 'all' in by and by['all']:
+            self.cache.clear()
+        elif 'id' in by and by['id'] in self.cache and self.cache_status:
+            del self.cache[by['id']]
+            del self.cache_time[by['id']]
+        elif 'old' in by and by['old']:
+            for record, time in self.cache.items():
+                if time + datetime.timedelta(seconds=self.cache_timeout_seconds) <= datetime.datetime.now():
+                    del self.cache[record]
+                    del self.cache_time[record]
+
+    def _cache_set(self, request):
+        """
+        Set cache with the id of each record as the key
+        :param request: (JSON) data
+        :return: (dictionary) of request, or (JSON) of request if it fails
+        """
+        try:
+            if len(request['data']) != 0 and self.cache_status:
+                # A single record
+                if type(request['data']) is dict:
+                    self.cache[request['data']['id']] = request['data']
+                    self.cache_time[request['data']['id']] = datetime.datetime.now()
+                    return request['data']
+                # A list of records
+                for record in request['data']:
+                    self.cache[record['id']] = record
+                    self.cache_time[record['id']] = datetime.datetime.now()
+                if len(request['data']) == 1:
+                    return request['data'][0]
+                else:
+                    return request['data']
+        except:
+            pass
+        return request
+
+    def _cache_get(self, id):
+        """
+        Retrieves from the cache the record, if cache is expire it will renew
+        :param id: (string) id of the record
+        :return: (dictionary) record
+        """
+        if id in self.cache and self.cache_status:
+            # Expired
+            if self.cache_time[id] + datetime.timedelta(seconds=self.cache_timeout_seconds) >= datetime.datetime.now():
+                return self.cache[id]
+            else:
+                # Expire record, delete cache and request new
+                self._cache_delete(id=id)
+                return self.get(id=id)
+
+    def create(self, **attributes):
+        """
+        Creates a record with given attributes
+        :param attributes: (**kwargs) fields with data you want to populate the record with.
+
+        :return: (dictionary) The record that was created with the attributes.
+        """
+        url = '/module'
+        data = {'type': self.module_name, 'id': str(uuid.uuid4()), 'attributes': attributes}
+        return self._cache_set(self.suitecrm.request(f'{self.suitecrm.baseurl}{url}', 'post', data))
+
+    def delete(self, id):
+        """
+        Delete a specific record by id.
+        :param id: (string) The record id within the module you want to delete.
+
+        :return: (dictionary) Confirmation of deletion of record.
+        """
+        # Delete
+        url = f'/module/{self.module_name}/{id}'
+        self._cache_delete(id=id)
+        return self.suitecrm.request(f'{self.suitecrm.baseurl}{url}', 'delete')
+
+    def fields(self):
+        """
+        Gets all the attributes that can be set in a record.
+        :return: (list) All the names of attributes in a record.
+        """
+        # Get total record count
+        url = f'/module/{self.module_name}?page[number]=1&page[size]=1'
+        return list(self.suitecrm.request(f'{self.suitecrm.baseurl}{url}', 'get')['data'][0]['attributes'].keys())
+
+    def get(self, fields=None, sort=None, **filters):
+        """
+        Gets records given a specific id or filters, can be sorted only once, and the fields returned for each record
+        can be specified.
+
+        :param fields: (list) A list of fields you want to be returned from each record.
+        :param sort: (string) The field you want the records to be sorted by.
+        :param filters: (**kwargs) fields that the record has that you want to filter on.
+
+        Important notice: we don’t support multiple level sorting right now!
+
+        :return: (dictionary/list) A list or dictionary of record(s) that meet the filter criteria.
+        """
+        # Fields Constructor
+        if fields:
+            fields = f'?fields[{self.module_name}]=' + ', '.join([fields])
+            url = f'/module/{self.module_name}{fields}&filter'
+        else:
+            url = f'/module/{self.module_name}?filter'
+
+        # Filter Constructor
+        for field, value in filters.items():
+            url = f'{url}[{field}][eq]={value}and&'
+        url = url[:-4]
+
+        # Sort
+        if sort:
+            url = f'{url}&sort=-{sort}'
+
+        # Execute
+        if 'id' in filters and len(filters) == 1 and filters['id'] in self.cache:
+            return self._cache_get(id=filters['id'])
+        else:
+            return self._cache_set(self.suitecrm.request(f'{self.suitecrm.baseurl}{url}', 'get'))
+
+    def get_all(self, record_per_page=100):
+        """
+        Gets all the records in the module.
+        :return: (dictionary) All the records within a module.
+        """
+        # Get total record count
+        url = f'/module/{self.module_name}?page[number]=1&page[size]=1'
+        pages = math.ceil(self.suitecrm.request(f'{self.suitecrm.baseurl}{url}', 'get')['meta']['total-pages'] /
+                          record_per_page) + 1
+        for page in range(1, pages):
+            url = f'/module/{self.module_name}?page[number]={page}&page[size]={record_per_page}'
+            self._cache_set(self.suitecrm.request(f'{self.suitecrm.baseurl}{url}', 'get'))
+
+        return self.cache
+
+    def update(self, id, **attributes):
+        """
+        updates a record.
+
+        :param id: (string) id of the current module record.
+        :param attributes: (**kwargs) fields inside of the record to be updated.
+
+        :return: (dictionary) The updated record
+        """
+        url = '/module'
+        data = {'type': self.module_name, 'id': id, 'attributes': attributes}
+        return self._cache_set(self.suitecrm.request(f'{self.suitecrm.baseurl}{url}', 'patch', data))
+
+    def get_relationship(self, id, related_module_name):
+        """
+        returns the relationship between this record and another module.
+
+        :param id: (string) id of the current module record.
+        :param related_module_name: (string) the module name you want to search relationships for, ie. Contacts.
+
+        :return: (dictionary) A list of relationships that this module's record contains with the related module.
+        """
+        url = f'/module/{self.module_name}/{id}/relationships/{related_module_name.lower()}'
+        return self.suitecrm.request(f'{self.suitecrm.baseurl}{url}', 'get')
+
+    def create_relationship(self, id, related_module_name, related_bean_id):
+        """
+        Creates a relationship between 2 records.
+
+        :param id: (string) id of the current module record.
+        :param related_module_name: (string) the module name of the record you want to create a relationship,
+               ie. Contacts.
+        :param related_bean_id: (string) id of the record inside of the other module.
+
+        :return: (dictionary) A record that the relationship was created.
+        """
+        # Post
+        url = f'/module/{self.module_name}/{id}/relationships'
+        data = {'type': related_module_name.capitalize(), 'id': related_bean_id}
+        return self.suitecrm.request(f'{self.suitecrm.baseurl}{url}', 'post', data)
+
+    def delete_relationship(self, id, related_module_name, related_bean_id):
+        """
+        Deletes a relationship between 2 records.
+
+        :param id: (string) id of the current module record.
+        :param related_module_name: (string) the module name of the record you want to delete a relationship,
+               ie. Contacts.
+        :param related_bean_id: (string) id of the record inside of the other module.
+
+        :return: (dictionary) A record that the relationship was deleted.
+        """
+        url = f'/module/{self.module_name}/{id}/relationships/{related_module_name.lower()}/{related_bean_id}'
+        return self.suitecrm.request(f'{self.suitecrm.baseurl}{url}', 'delete')
